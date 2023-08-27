f554ebed0feb36ee86c4e6ebaa70b670cae0070b
Dan Crankshaw <crankshaw@google.com>
2023-08-27 01:45:00 +0000

    fix bug

--- pretrain/redrock_trainer.py
+++ pretrain/redrock_trainer.py
@@ -13,8 +13,8 @@ from lightning.pytorch.strategies import FSDPStrategy, XLAStrategy
 from torch.utils.data import DataLoader, IterableDataset
 import torch.autograd.profiler
 import torch.multiprocessing as mp
-import nvidia_dlprof_pytorch_nvtx
-nvidia_dlprof_pytorch_nvtx.init()
+# import nvidia_dlprof_pytorch_nvtx
+# nvidia_dlprof_pytorch_nvtx.init()
 
 
 # support running without installing as a package
