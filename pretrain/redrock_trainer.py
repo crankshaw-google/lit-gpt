f61be86c03b45c0f6541b803dee63e4c7c368cc2
Dan Crankshaw <crankshaw@google.com>
2023-08-27 17:44:19 +0000

    Make pt profiler configurable and run for global batches

--- pretrain/redrock_trainer.py
+++ pretrain/redrock_trainer.py
@@ -1,24 +1,22 @@
+from contextlib import nullcontext
 import math
+from pathlib import Path
 import sys
 import time
-from pathlib import Path
 from typing import Any, Optional
 
 import lightning as L
-import numpy as np
-import torch
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers import CSVLogger
 from lightning.pytorch.profilers import PyTorchProfiler
 from lightning.pytorch.strategies import FSDPStrategy, XLAStrategy
-from torch.utils.data import DataLoader, IterableDataset
+import numpy as np
+import torch
 import torch.autograd.profiler
-import torch.multiprocessing as mp
 from torch.distributed.fsdp import ShardingStrategy
+import torch.multiprocessing as mp
 import torch.profiler as tprofiler
-from contextlib import nullcontext
-# import nvidia_dlprof_pytorch_nvtx
-# nvidia_dlprof_pytorch_nvtx.init()
+from torch.utils.data import DataLoader, IterableDataset
 
 
 # support running without installing as a package
@@ -30,7 +28,7 @@ from lit_gpt.model import GPT, Block
 from lit_gpt.speed_monitor import SpeedMonitorCallback, estimate_flops, measure_flops
 from lit_gpt.utils import chunked_cross_entropy, get_default_supported_precision, step_csv_logger
 
-mp.set_start_method('spawn', force=True)
+mp.set_start_method("spawn", force=True)
 
 import utilities.monitor_collectives
 
@@ -52,72 +50,107 @@ warmup_iters = 2000
 lr_decay_iters = max_iters
 min_lr = 6e-5
 
-hparams = {k: v for k, v in locals().items() if isinstance(v, (int, float, str)) and not k.startswith("_")}
+hparams = {
+    k: v
+    for k, v in locals().items()
+    if isinstance(v, (int, float, str)) and not k.startswith("_")
+}
 
 
 class LightningGPTModule(L.LightningModule):
-    def __init__(self, config: Config, micro_batch_size, prof: Optional[tprofiler.profile]) -> None:
-        super().__init__()
-        self.config = config
-        self.module: Optional[torch.nn.Module] = None
-        self.measured_flops: Optional[int] = None
-        self.nsys_profile_step_multiple = 5
-        self.micro_batch_size = micro_batch_size
-        self.prof = prof
 
-    def configure_model(self) -> None:
-        self.module = GPT(self.config)
-        self.module.apply(self.module._init_weights)
+  def __init__(
+      self,
+      config: Config,
+      micro_batch_size,
+      prof: Optional[tprofiler.profile],
+      gradient_accumulation_steps: int,
+  ) -> None:
+    super().__init__()
+    self.config = config
+    self.module: Optional[torch.nn.Module] = None
+    self.measured_flops: Optional[int] = None
+    self.nsys_profile_step_multiple = 5
+    self.micro_batch_size = micro_batch_size
+    self.prof = prof
+    self.gradient_accumulation_steps = gradient_accumulation_steps
 
-    def configure_optimizers(self) -> torch.optim.Optimizer:
-        return torch.optim.AdamW(
-            self.module.parameters(), lr=learning_rate, weight_decay=weight_decay, betas=(beta1, beta2), foreach=False
-        )
+  def configure_model(self) -> None:
+    self.module = GPT(self.config)
+    self.module.apply(self.module._init_weights)
 
-    def on_fit_start(self) -> None:
-        trainer = self.trainer
-        with torch.device("meta"):
-            meta_model = GPT(self.module.config)
-            # "estimated" is not as precise as "measured". Estimated is optimistic but widely used in the wild.
-            # When comparing MFU or FLOP numbers with other projects that use estimated FLOPs,
-            # consider setting `self.measured_flops = estimated_flops` instead
-            estimated_flops = estimate_flops(meta_model) * self.micro_batch_size
-            self.print(f"Estimated TFLOPs: {estimated_flops * trainer.world_size / 1e12:.2f}")
-            x = torch.randint(0, 1, (self.micro_batch_size, meta_model.config.block_size))
-            self.measured_flops = measure_flops(meta_model, x)
-            self.print(f"Measured TFLOPs: {self.measured_flops * trainer.world_size / 1e12:.2f}")
+  def configure_optimizers(self) -> torch.optim.Optimizer:
+    return torch.optim.AdamW(
+        self.module.parameters(),
+        lr=learning_rate,
+        weight_decay=weight_decay,
+        betas=(beta1, beta2),
+        foreach=False,
+    )
 
-    def on_train_batch_start(self, batch: Any, batch_idx: int) -> None:
-        if not decay_lr:
-            return
-        # determine and set the learning rate for this iteration
-        lr = get_lr(self.trainer.fit_loop.total_batch_idx)
-        for optimizer in self.trainer.strategy.optimizers:
-            for param_group in optimizer.param_groups:
-                param_group["lr"] = lr
+  def on_fit_start(self) -> None:
+    trainer = self.trainer
+    with torch.device("meta"):
+      meta_model = GPT(self.module.config)
+      # "estimated" is not as precise as "measured". Estimated is optimistic but widely used in the wild.
+      # When comparing MFU or FLOP numbers with other projects that use estimated FLOPs,
+      # consider setting `self.measured_flops = estimated_flops` instead
+      estimated_flops = estimate_flops(meta_model) * self.micro_batch_size
+      self.print(
+          f"Estimated TFLOPs: {estimated_flops * trainer.world_size / 1e12:.2f}"
+      )
+      x = torch.randint(
+          0, 1, (self.micro_batch_size, meta_model.config.block_size)
+      )
+      self.measured_flops = measure_flops(meta_model, x)
+      self.print(
+          "Measured TFLOPs:"
+          f" {self.measured_flops * trainer.world_size / 1e12:.2f}"
+      )
 
-        if batch_idx > 0 and batch_idx % self.nsys_profile_step_multiple == 0:
-            torch.cuda.cudart().cudaProfilerStart()
+  def on_train_batch_start(self, batch: Any, batch_idx: int) -> None:
+    if not decay_lr:
+      return
+    # determine and set the learning rate for this iteration
+    lr = get_lr(self.trainer.fit_loop.total_batch_idx)
+    for optimizer in self.trainer.strategy.optimizers:
+      for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
 
+    global_batch_idx = batch_idx / self.gradient_accumulation_steps
+    if (
+        global_batch_idx > 0
+        and global_batch_idx % self.nsys_profile_step_multiple == 0
+    ):
+      torch.cuda.cudart().cudaProfilerStart()
 
-    def on_train_batch_end(self, outputs, batch: Any, batch_idx: int, unused: int = 0) -> None:
-        if self.prof:
-            self.prof.step()
-        if batch_idx > 2 and batch_idx % self.nsys_profile_step_multiple == 2:
-            torch.cuda.cudart().cudaProfilerStop()
+  def on_train_batch_end(
+      self, outputs, batch: Any, batch_idx: int, unused: int = 0
+  ) -> None:
+    global_batch_idx = batch_idx / self.gradient_accumulation_steps
+    global_batch_offset = batch_idx % self.gradient_accumulation_steps
+    if self.prof and batch_idx > 0 and global_batch_offset == 0:
+      self.prof.step()
+    if (
+        global_batch_idx > 1
+        and global_batch_idx % self.nsys_profile_step_multiple == 0
+    ):
+      torch.cuda.cudart().cudaProfilerStop()
+    if global_batch_offset == 0:
+      self.print(f"HEARTBEAT: {global_batch_idx=}, {batch_idx=}")
 
-    def training_step(self, batch: Any, batch_idx: int) -> torch.Tensor:
-        input_ids, targets = batch
-        logits = self.module(input_ids)
-        loss = chunked_cross_entropy(logits, targets, chunk_size=0)
-        self.log("train_loss", loss, on_step=True, on_epoch=False, prog_bar=True)
-        return loss
+  def training_step(self, batch: Any, batch_idx: int) -> torch.Tensor:
+    input_ids, targets = batch
+    logits = self.module(input_ids)
+    loss = chunked_cross_entropy(logits, targets, chunk_size=0)
+    self.log("train_loss", loss, on_step=True, on_epoch=False, prog_bar=True)
+    return loss
 
-    def validation_step(self, batch: Any, batch_idx: int) -> None:
-        input_ids, targets = batch
-        logits = self.module(input_ids)
-        loss = chunked_cross_entropy(logits, targets, chunk_size=0)
-        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+  def validation_step(self, batch: Any, batch_idx: int) -> None:
+    input_ids, targets = batch
+    logits = self.module(input_ids)
+    loss = chunked_cross_entropy(logits, targets, chunk_size=0)
+    self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
 
 
 def main(
@@ -131,137 +164,174 @@ def main(
     num_nodes: int = 1,
     batch_size: int = 512,
     micro_batch_size: int = 4,
-    use_profiler: bool = False
+    use_pt_profiler: bool = False,
+    pt_profiler_wait: int = 1,
+    pt_profiler_warmup: int = 2,
+    pt_profiler_active: int = 2,
+    pt_profiler_repeat: int = 5,
 ) -> None:
-    if use_profiler:
-      cm = nullcontext()
+  if use_profiler:
+    cm = nullcontext()
+  else:
+    cm = torch.autograd.profiler.emit_nvtx()
+  with cm:
+    precision = precision or get_default_supported_precision(
+        training=True, tpu=tpu
+    )
+
+    base_out_dir = Path(out_dir)
+    logger_out_dir = base_out_dir / "csv_logger"
+    checkpoint_out_dir = base_out_dir / "checkpoints"
+    tprofiler_out_dir = base_out_dir / "tprofiler"
+    data_dir = Path(data_dir)
+
+    gradient_accumulation_steps = batch_size // micro_batch_size
+    assert gradient_accumulation_steps > 0
+    if devices > 1:
+      if tpu:
+        # For multi-host TPU training, the device count for Fabric is limited to the count on a single host.
+        devices = "auto"
+        strategy = XLAStrategy(sync_module_states=False)
+      else:
+        strategy = FSDPStrategy(
+            auto_wrap_policy={Block},
+            activation_checkpointing_policy={Block},
+            # the argument is not available in the Trainer strategy, but it's the default anyways
+            # state_dict_type="full",
+            limit_all_gathers=True,
+            cpu_offload=False,
+            sharding_strategy=ShardingStrategy.FULL_SHARD,
+        )
     else:
-      cm = torch.autograd.profiler.emit_nvtx()
-    with cm:
-        precision = precision or get_default_supported_precision(training=True, tpu=tpu)
+      strategy = "auto"
 
-        base_out_dir = Path(out_dir)
-        logger_out_dir = base_out_dir / "csv_logger"
-        checkpoint_out_dir = base_out_dir / "checkpoints"
-        tprofiler_out_dir = base_out_dir / "tprofiler"
-        data_dir = Path(data_dir)
+    logger = step_csv_logger(
+        str(logger_out_dir),
+        name,
+        cls=CSVLogger,
+        flush_logs_every_n_steps=log_interval,
+    )
+    speed_monitor = SpeedMonitorCallback(
+        length_fn=lambda batch: batch[0].size(1),
+        batch_size=micro_batch_size,
+        window_size=50,
+        time_unit="seconds",
+    )
+    model_checkpoint = ModelCheckpoint(
+        dirpath=checkpoint_out_dir,
+        every_n_train_steps=save_interval,
+        save_last=True,
+        verbose=True,
+    )
+    trainer = L.Trainer(
+        devices=devices,
+        strategy=strategy,
+        precision=precision,
+        logger=logger,
+        callbacks=[speed_monitor, model_checkpoint],
+        max_steps=max_iters,
+        max_epochs=1,
+        limit_val_batches=eval_iters,
+        accumulate_grad_batches=gradient_accumulation_steps,
+        log_every_n_steps=log_interval,
+        val_check_interval=eval_interval,
+        num_nodes=num_nodes,
+    )
 
-        gradient_accumulation_steps = batch_size // micro_batch_size
-        assert gradient_accumulation_steps > 0
-        if devices > 1:
-            if tpu:
-                # For multi-host TPU training, the device count for Fabric is limited to the count on a single host.
-                devices = "auto"
-                strategy = XLAStrategy(sync_module_states=False)
-            else:
-                strategy = FSDPStrategy(
-                    auto_wrap_policy={Block},
-                    activation_checkpointing_policy={Block},
-                    # the argument is not available in the Trainer strategy, but it's the default anyways
-                    # state_dict_type="full",
-                    limit_all_gathers=True,
-                    cpu_offload=False,
-                    sharding_strategy=ShardingStrategy.FULL_SHARD,
-                )
-        else:
-            strategy = "auto"
+    L.seed_everything(
+        1337, workers=True
+    )  # same seed for every process to init model (FSDP)
 
-        logger = step_csv_logger(str(logger_out_dir), name, cls=CSVLogger, flush_logs_every_n_steps=log_interval)
-        speed_monitor = SpeedMonitorCallback(
-            length_fn=lambda batch: batch[0].size(1), batch_size=micro_batch_size, window_size=50, time_unit="seconds"
-        )
-        model_checkpoint = ModelCheckpoint(dirpath=checkpoint_out_dir, every_n_train_steps=save_interval, save_last=True, verbose=True)
-        # profiler = PyTorchProfiler(dirpath=out_dir, emit_nvtx=True, export_to_chrome=True)
-        trainer = L.Trainer(
-            devices=devices,
-            strategy=strategy,
-            precision=precision,
-            logger=logger,
-            callbacks=[speed_monitor, model_checkpoint],
-            max_steps=max_iters,
-            max_epochs=1,
-            limit_val_batches=eval_iters,
-            accumulate_grad_batches=gradient_accumulation_steps,
-            log_every_n_steps=log_interval,
-            val_check_interval=eval_interval,
-            num_nodes=num_nodes,
-            # profiler=profiler,
-        )
+    trainer.print(hparams)
 
-        L.seed_everything(1337, workers=True)  # same seed for every process to init model (FSDP)
+    if trainer.global_rank == 0:
+      base_out_dir.mkdir(parents=True, exist_ok=True)
+      checkpoint_out_dir.mkdir(parents=True, exist_ok=True)
+      logger_out_dir.mkdir(parents=True, exist_ok=True)
 
-        trainer.print(hparams)
+    config = Config.from_name(model_name)
+    trainer.print(f"Loading model with {config.__dict__}")
+    t0 = time.perf_counter()
+    if use_profiler:
+      prof = tprofiler.profile(
+          schedule=tprofiler.schedule(
+              wait=pr_profiler_wait,
+              warmup=pt_profiler_warmup,
+              active=pt_profiler_active,
+              repeat=pt_profiler_repeat,
+          ),
+          on_trace_ready=tprofiler.tensorboard_trace_handler(tprofiler_out_dir),
+          record_shapes=True,
+          with_stack=True,
+      )
+      prof.start()
+    else:
+      prof = None
+    model = LightningGPTModule(
+        config, micro_batch_size, prof, gradient_accumulation_steps
+    )
+    trainer.print(
+        f"Time to instantiate model: {time.perf_counter() - t0:.02f} seconds."
+    )
 
-        if trainer.global_rank == 0:
-            base_out_dir.mkdir(parents=True, exist_ok=True)
-            checkpoint_out_dir.mkdir(parents=True, exist_ok=True)
-            logger_out_dir.mkdir(parents=True, exist_ok=True)
+    train_data = Dataset(str(data_dir / "train.bin"), config.block_size)
+    val_data = Dataset(str(data_dir / "val.bin"), config.block_size)
+    train_dataloader = DataLoader(
+        train_data, batch_size=micro_batch_size, num_workers=2
+    )
+    val_dataloader = DataLoader(
+        val_data, batch_size=micro_batch_size, num_workers=2
+    )
 
-        config = Config.from_name(model_name)
-        trainer.print(f"Loading model with {config.__dict__}")
-        t0 = time.perf_counter()
-        if use_profiler:
-          prof = tprofiler.profile(
-              schedule=tprofiler.schedule(wait=32, warmup=32, active=64, repeat=3),
-              on_trace_ready=tprofiler.tensorboard_trace_handler(tprofiler_out_dir),
-              record_shapes=True,
-              with_stack=True
-          )
-          prof.start()
-        else:
-          prof = None
-        model = LightningGPTModule(config, micro_batch_size, prof)
-        trainer.print(f"Time to instantiate model: {time.perf_counter() - t0:.02f} seconds.")
-
-        train_data = Dataset(str(data_dir / "train.bin"), config.block_size)
-        val_data = Dataset(str(data_dir / "val.bin"), config.block_size)
-        train_dataloader = DataLoader(train_data, batch_size=micro_batch_size, num_workers=2)
-        val_dataloader = DataLoader(val_data, batch_size=micro_batch_size, num_workers=2)
-
-        t0 = time.perf_counter()
-        trainer.fit(model, train_dataloader, val_dataloader, ckpt_path="last")
-        trainer.print(f"Training time: {(time.perf_counter()-t0):.2f}s")
-        if trainer.strategy.root_device.type == "cuda":
-            trainer.print(f"Memory used: {torch.cuda.max_memory_allocated() / 1e9:.02f} GB")
-        if prof:
-          prof.stop()
+    t0 = time.perf_counter()
+    trainer.fit(model, train_dataloader, val_dataloader, ckpt_path="last")
+    trainer.print(f"Training time: {(time.perf_counter()-t0):.2f}s")
+    if trainer.strategy.root_device.type == "cuda":
+      trainer.print(
+          f"Memory used: {torch.cuda.max_memory_allocated() / 1e9:.02f} GB"
+      )
+    if prof:
+      prof.stop()
 
 
 class Dataset(IterableDataset):
-    def __init__(self, data_file: Path, block_size: int):
-        super().__init__()
-        self.data_file = data_file
-        self.block_size = block_size
 
-    def __iter__(self):
-        data = np.memmap(self.data_file, dtype=np.uint16, mode="r")
-        while True:
-            i = torch.randint(len(data) - self.block_size, (1,)).item()
-            x = torch.from_numpy((data[i : i + self.block_size]).astype(np.int64))
-            y = torch.from_numpy((data[i + 1 : i + 1 + self.block_size]).astype(np.int64))
-            yield x, y
+  def __init__(self, data_file: Path, block_size: int):
+    super().__init__()
+    self.data_file = data_file
+    self.block_size = block_size
+
+  def __iter__(self):
+    data = np.memmap(self.data_file, dtype=np.uint16, mode="r")
+    while True:
+      i = torch.randint(len(data) - self.block_size, (1,)).item()
+      x = torch.from_numpy((data[i : i + self.block_size]).astype(np.int64))
+      y = torch.from_numpy(
+          (data[i + 1 : i + 1 + self.block_size]).astype(np.int64)
+      )
+      yield x, y
 
 
 # learning rate decay scheduler (cosine with warmup)
 def get_lr(it):
-    # 1) linear warmup for warmup_iters steps
-    if it < warmup_iters:
-        return learning_rate * it / warmup_iters
-    # 2) if it > lr_decay_iters, return min learning rate
-    if it > lr_decay_iters:
-        return min_lr
-    # 3) in between, use cosine decay down to min learning rate
-    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
-    assert 0 <= decay_ratio <= 1
-    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))  # coeff ranges 0..1
-    return min_lr + coeff * (learning_rate - min_lr)
+  # 1) linear warmup for warmup_iters steps
+  if it < warmup_iters:
+    return learning_rate * it / warmup_iters
+  # 2) if it > lr_decay_iters, return min learning rate
+  if it > lr_decay_iters:
+    return min_lr
+  # 3) in between, use cosine decay down to min learning rate
+  decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+  assert 0 <= decay_ratio <= 1
+  coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))  # coeff ranges 0..1
+  return min_lr + coeff * (learning_rate - min_lr)
 
 
 if __name__ == "__main__":
-    # Uncomment this line if you see an error: "Expected is_sm80 to be true, but got false"
-    # torch.backends.cuda.enable_flash_sdp(False)
-    torch.set_float32_matmul_precision("high")
+  # Uncomment this line if you see an error: "Expected is_sm80 to be true, but got false"
+  # torch.backends.cuda.enable_flash_sdp(False)
+  torch.set_float32_matmul_precision("high")
 
-    from jsonargparse import CLI
+  from jsonargparse import CLI
 
-    CLI(main)
+  CLI(main)
